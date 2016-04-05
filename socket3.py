d9f8a523f315d5dceb00d8be71fe3779868c9c0f
AliTATLICI <alitatlici@sdu.edu.tr>
2016-04-05 10:22:50 +0300

    Update socket3.py

--- socket3.py
+++ socket3.py
@@ -5,9 +5,9 @@ from socket import *
 
 def is_up(addr):
     s = socket(AF_INET, SOCK_STREAM)
-    s.settimeout(0.01)    ## set a timeout of 0.01 sec
-    if not s.connect_ex((addr,135)):    # connect to the remote host on port 135
-        s.close()                       ## (port 135 is always open on Windows machines, AFAIK)
+    s.settimeout(0.01)   
+    if not s.connect_ex((addr,135)):    
+        s.close()                     
         return 1
     else:
         s.close()
@@ -16,14 +16,13 @@ def run(ipsi):
     network = ipsi
     ip_listesi=[]
     print('')
-    for ip in range(1,256):    ## 'ping' addresses 192.168.1.1 to .1.255
+    for ip in range(1,256):    ## 'ping' adresi 192.168.1.1 ile .1.255 arası
         addr = network + str(ip)
         if is_up(addr):
             ip_listesi.append(addr)
-            print('%s \t- %s' %(addr, getfqdn(addr)))    ## the function 'getfqdn' returns the remote hostname
+            print('%s \t- %s' %(addr, getfqdn(addr)))    
     return ip_listesi
-    #print("")    ## just print a blank line
-
+    
 
 if __name__ == '__main__':
    
